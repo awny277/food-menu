c375b7e4acc3dacfdc0208adddfc87c59298a7a6
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-12-08 00:18:19 +0200

    update

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { BiLogIn } from "react-icons/bi";
+import { BsBagDash } from "react-icons/bs";
 import styles from "../../styles/NavBar.module.css";
 const Navbar = () => {
-  return <div className={styles.navber}>Hello from Navbar</div>;
+  return (
+    <div className={styles.navber}>
+      <Container>
+        <div className={styles.content}>
+          <h2>
+            Food <br /> <span>Empire</span>
+          </h2>
+          <div className={styles.Links}>
+            <span>Favorite Fruits</span>
+            <span>Fruit Shoop</span>
+            <span>About Us</span>
+            <div className={styles.icons}>
+              <span>
+                <BiLogIn />
+              </span>
+              <span>
+                <BsBagDash />
+              </span>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
