3d8937d83d8b1af9372d28c45e711d27e74fc1f7
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-12-07 20:31:49 +0200

    update

--- src/components/pages/Home/About/About.js
+++ src/components/pages/Home/About/About.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { HiOutlineArrowSmRight, HiOutlineArrowSmLeft } from "react-icons/hi";
+
+import styles from "../../../styles/About.module.css";
 
 const About = () => {
-  return <></>;
+  return (
+    <div className={styles.about}>
+      <Container>
+        <div className={styles.header}>
+          <h2>Abouts Us</h2>
+          <span>
+            <HiOutlineArrowSmLeft />
+            <HiOutlineArrowSmRight />
+          </span>
+        </div>
+        {/* <Row>{data}</Row> */}
+      </Container>
+    </div>
+  );
 };
 
 export default About;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductCard from "../../Layout/ProductCard/ProductCard";
 import styles from "../../styles/Home.module.css";
+import About from "./About/About";
 import Favoutrie from "./Favoutrie/Favoutrie";
 import FruiteShop from "./FruiteShop/FruiteShop";
 import Header from "./Header/Header";
@@ -11,6 +12,7 @@ const Home = () => {
       <Favoutrie />
       <ProductCard />
       <FruiteShop />
+      <About />
     </>
   );
 };
