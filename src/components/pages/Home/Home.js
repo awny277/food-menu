454b6ed66ce9a4c61d8e8799e8d58c788ac8dd61
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-12-23 21:43:16 +0200

    update

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,19 +1,29 @@
-import React from "react";
-import ProductCard from "../../Layout/ProductCard/ProductCard";
-import About from "./About/About";
-import Favoutrie from "./Favoutrie/Favoutrie";
-import FruiteShop from "./FruiteShop/FruiteShop";
-import Header from "./Header/Header";
-import Subscribe from "./Subscribe/Subscribe";
+import React, { Suspense } from "react";
+// import ProductCard from "../../Layout/ProductCard/ProductCard";
+// import About from "./About/About";
+// import Favoutrie from "./Favoutrie/Favoutrie";
+// import FruiteShop from "./FruiteShop/FruiteShop";
+// import Header from "./Header/Header";
+// import Subscribe from "./Subscribe/Subscribe";
+const ProductCard = React.lazy(() =>
+  import("../../Layout/ProductCard/ProductCard")
+);
+const About = React.lazy(() => import("./About/About"));
+const Favoutrie = React.lazy(() => import("./Favoutrie/Favoutrie"));
+const FruiteShop = React.lazy(() => import("./FruiteShop/FruiteShop"));
+const Header = React.lazy(() => import("./Header/Header"));
+const Subscribe = React.lazy(() => import("./Subscribe/Subscribe"));
 const Home = () => {
   return (
     <>
-      <Header />
-      <Favoutrie />
-      <ProductCard />
-      <FruiteShop />
-      <About />
-      <Subscribe />
+      <Suspense fallback={<></>}>
+        <Header />
+        <Favoutrie />
+        <ProductCard />
+        <FruiteShop />
+        <About />
+        <Subscribe />
+      </Suspense>
     </>
   );
 };
